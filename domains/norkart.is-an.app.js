75f54a78f3573f09f1d9b486a99d1ce2c6790ae4
Nork Art <138743746+norkart1@users.noreply.github.com>
2023-12-05 21:07:07 +0530

    Create norkart.is-an.app.js

---
+++ domains/norkart.is-an.app.js
@@ -0,0 +1,15 @@
+addSubDomain({
+    description: 'Website for Developer news.',
+    domain: 'is-an.app',
+    subdomain: 'norkart',
+    owner: {
+      repo: 'https://github.com/norkart1/norkartwebsite',
+      email: 'yopremium21@gmail.com',
+    },
+    record: {
+      CNAME: 'norkart1.github.io',
+    },
+  })
+
+  
+
